6a2d3956894b79b36b0239bdcfd76210cd9bba83
snrredoy <snrredoy3@gmail.com>
2025-07-29 17:33:45 +0600

    Add created_at field and JSON encoder to UserProfile model

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
-from pydantic import BaseModel, Field, field_validator, model_validator, computed_field
+from pydantic import BaseModel, Field, field_validator, model_validator, computed_field, ConfigDict
 from typing import Optional, List, Dict, Set
+from datetime import datetime
 
 class Cart(BaseModel):
     user_id : int
@@ -72,6 +73,13 @@ class UserProfile(BaseModel):
     id : int
     full_name: str
     address: Address
+    created_at: datetime
+
+    model_config = ConfigDict(
+        json_encoders = {
+            datetime: lambda v: v.strftime('%d-%m-%Y %H:%M:%S')
+        }
+    )
 
 
 class Comment(BaseModel):
@@ -81,3 +89,24 @@ class Comment(BaseModel):
     replies: Optional[List['Comment']] = None
 
 Comment.model_rebuild()  # To handle recursive type definition forward referencing
+
+
+user = UserProfile(
+    id=1,
+    full_name="John Smith",
+    created_at=datetime(2023, 10, 1, 12, 0, 0),
+    address=Address(
+        street="123 Main St",
+        city="Anytown",
+        state="CA",
+        zip_code="12345"
+    )
+)
+
+# using model_dump
+user_dict = user.model_dump()
+print(user_dict)
+
+# using model_dump_json
+user_json = user.model_dump_json()
+print(user_json)

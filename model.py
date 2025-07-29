cec9c46ef998caf3432363a81a62968497c1e862
snrredoy <snrredoy3@gmail.com>
2025-07-29 15:53:00 +0600

    Add Cart and BlogPost models to the Pydantic schema

--- model.py
+++ model.py
@@ -1,5 +1,16 @@
 from pydantic import BaseModel, Field
-from typing import Optional
+from typing import Optional, List, Dict, Set
+
+class Cart(BaseModel):
+    user_id : int
+    items: List[str]
+    quantity: Dict[str, int]
+
+class BlogPost(BaseModel):
+    title: str
+    description: str
+    image_url: Optional[str] = None
+
 
 class User(BaseModel):
     username: str = Field(
